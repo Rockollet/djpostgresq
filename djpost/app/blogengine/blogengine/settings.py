19603b003041ad51a3838be65be6f2b86a75f177
Rockollet <rockollet@gmail.com>
2019-09-10 20:18:20 +0300

    404

--- djpost/app/blogengine/blogengine/settings.py
+++ djpost/app/blogengine/blogengine/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f12vig@&z^hn6^m18r9x(ou=fj08aj(v9)1u=3n6i4r#%1l(qd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['rocko.dmitriyq.ru', 'jetblogit.ru']
+ALLOWED_HOSTS = ['jetblogit.ru']
 
 
 # Application definition
