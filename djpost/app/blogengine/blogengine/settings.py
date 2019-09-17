dcc41915d4b66954ebebbd0a1299e1238e4555f1
Rockollet <rockollet@gmail.com>
2019-09-18 00:07:58 +0300

    DEBUG = TEMPLATE_DEBUG = True else:   DEBUG = TEMPLATE_DEBUG = False

--- djpost/app/blogengine/blogengine/settings.py
+++ djpost/app/blogengine/blogengine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import socket
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,9 +26,12 @@ SECRET_KEY = 'f12vig@&z^hn6^m18r9x(ou=fj08aj(v9)1u=3n6i4r#%1l(qd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+if socket.gethostname() == '127.0.0.1:5000':
+    DEBUG = TEMPLATE_DEBUG = True
+else:
+    DEBUG = TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = ['jetblogit.ru']
+ALLOWED_HOSTS = ['jetblogit.ru', '127.0.0.1']
 
 
 # Application definition
