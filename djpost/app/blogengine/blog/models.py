acb16591b77a6cc182e2f2f51b0144ff3f09cbfa
Rockollet <rockollet@gmail.com>
2019-10-08 19:34:14 +0300

    push img blank=True

--- djpost/app/blogengine/blog/models.py
+++ djpost/app/blogengine/blog/models.py
@@ -4,7 +4,6 @@ from PIL import Image
 from django.utils.text import slugify
 from time import time
 
-
 def gen_slug(s):
     new_slug = slugify(s, allow_unicode=True)
     return new_slug + '-' + str(int(time()))
