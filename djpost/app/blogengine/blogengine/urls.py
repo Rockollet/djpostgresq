d098bbbbc7648a4fadb4a3c3a4ffcb7b59ee7cc3
Rockollet <rockollet@gmail.com>
2019-10-09 22:50:48 +0300

    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

--- djpost/app/blogengine/blogengine/urls.py
+++ djpost/app/blogengine/blogengine/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
