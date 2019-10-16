e0846f75d1e29c2a91223fe90d7e6179ee306289
Rockollet <rockollet@gmail.com>
2019-10-16 23:25:06 +0300

    blank=True

--- djpost/app/blogengine/blog/models.py
+++ djpost/app/blogengine/blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     body = models.TextField(blank=True, db_index=True)
     tags = models.ManyToManyField('Tag', blank=True , related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='static/media', blank=True, null=True)
+    image = models.ImageField(upload_to='static/media', blank=True)
 
 
     def get_absolute_url(self):
