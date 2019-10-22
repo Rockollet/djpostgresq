18ed9480ebb33469dab6d14224b1505c62d82e07
Rockollet <51819097+Rockollet@users.noreply.github.com>
2019-10-22 12:49:16 +0300

    Update settings.py

--- djpost/app/blogengine/blogengine/settings.py
+++ djpost/app/blogengine/blogengine/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
-ADMIN_MEDIA_PREFIX = '/admin/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 PATH_ENVS = [PROJECT_ROOT, STATIC_ROOT, STATIC_URL, MEDIA_ROOT, MEDIA_URL]
 
