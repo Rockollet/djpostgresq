be32ed355e82c5df92c963a5e61ed83dc0a38a41
Rockollet <rockollet@gmail.com>
2019-12-10 23:49:51 +0300

    IndentationError: expected an indented block

--- djpost/app/blogengine/blog/templatetags/adsense_tags.py
+++ djpost/app/blogengine/blog/templatetags/adsense_tags.py
@@ -6,19 +6,18 @@ register = template.Library()
 @register.filter
 def inject_adsense_after_paragraph(value, arg):
 
-  # Render our adsense code placed in html file
-  ad_code = render_to_string("blog/adsense_ads.html")
+    # Render our adsense code placed in html file
+    ad_code = render_to_string("blog/adsense_ads.html")
 
-  # Break down content into paragraphs
-  paragraphs = value.split('</p>')
+    # Break down content into paragraphs
+    paragraphs = value.split('</p>')
 
-  # Check if paragraph we want to post after exists
-  if arg < len(paragraphs):
+    # Check if paragraph we want to post after exists
+    if arg < len(paragraphs):
+        # Append our code before the following paragraph
+        paragraphs[arg] = ad_code + paragraphs[arg]
 
-  # Append our code before the following paragraph
-  paragraphs[arg] = ad_code + paragraphs[arg]
+        # Assemble our text back with injected adsense code
+        value = '</p>'.join(paragraphs)
 
-# Assemble our text back with injected adsense code
-  value = '</p>'.join(paragraphs)
-
-  return value
+        return value
